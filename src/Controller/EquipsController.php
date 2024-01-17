0c822eeb701388423609c1e5979457e9fadef95a
Jaume-tuset <tusetjaume7@gmail.com>
2024-01-17 22:23:34 +0100

    Commit Twig EquipsJaume

--- src/Controller/EquipsController.php
+++ src/Controller/EquipsController.php
@@ -17,28 +17,23 @@ class EquipsController extends AbstractController
    );
 
 
-   #[Route('/equip/{codi}', name: 'dades_equip')]
-   public function dades($codi)
-   {
-       $resultat = array_filter($this->equips, function ($dades) use ($codi) {
-           return $dades["codi"] == $codi;
-       });
+   #[Route('/equip/{codi<\d+>?1}',name:'dades_equip')]
+    public function dades($codi){
+       $resultat = array_filter(
+           $this->equips,
+           function ($dades) use ($codi) {
+               return $dades["codi"]==$codi;
+           }
+       );
 
 
-       if (count($resultat) > 0) {
-           $resposta = "";
-           $resultat = array_shift($resultat);
-           $mb = implode(" ", $resultat["membres"]);
-           $resposta .= "<ul><li>" . $resultat["nom"] . "</li>" .
-               "<li>" . $resultat["cicle"] . "</li>" .
-               "<li>" . $resultat["curs"] . "</li>" .
-               "<li>" . $mb . "</li></ul>";
-               
-           return new Response("<html><body>$resposta</body></html>");
-       } else {
-           return new Response("No s'ha trobat l'equip : " . $codi);
+       if(count($resultat)>0){
+           return $this->render('dades_equip.html.twig', array('dades' => array_shift($resultat)));
+       }else{
+           return $this->render('dades_equip.html.twig', array('dades' => array_shift($resultat)));
        }
-   }
+    }
+
 }
 
 
